dce109d2545a03eaade27658a5a12faaf8ac1d3d
okans <okans@okans-MacBook-Pro.local>
2021-08-09 14:59:07 +0300

    fixed due to heroku deployment

--- productapi/urls.py
+++ productapi/urls.py
@@ -20,4 +20,5 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('', include('api.urls')), # I did this because of heroku's homepage
 ]
